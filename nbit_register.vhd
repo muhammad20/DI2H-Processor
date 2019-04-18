536b5669073eaba07a946fa05bce17a89a6b5298
muhammad20 <muhammadahmad201196@hotmail.com>
2019-04-18 18:29:50 +0200

    added generic register

---
+++ nbit_register.vhd
@@ -0,0 +1,25 @@
+Library ieee;
+
+use ieee.std_logic_1164.all;
+
+
+entity nbit_register is
+generic(n: integer := 16);
+port(
+    indata: in std_logic_vector(n-1 downto 0);
+    clk, rst, en: in std_logic;
+    outData: out std_logic_vector(n-1 downto 0));
+end entity;
+
+architecture registerarch of nbit_register is
+    begin
+        process(clk, rst, en, indata)
+        begin
+            if(rst = '1' and en = '1') 
+                then outData <= (others => '0');
+            elsif (rising_edge(clk) and en = '1')
+                then outData <= inData;
+            end if;
+        end process;
+end architecture;
+
