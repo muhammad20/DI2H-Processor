690ab11e364f152445eca2206f4f7799ddebbbb6
muhammad20 <muhammadahmad201196@hotmail.com>
2019-04-18 18:29:10 +0200

    added generic tristate

---
+++ tristate.vhd
@@ -0,0 +1,18 @@
+Library ieee;
+
+use ieee.std_logic_1164.all;
+
+entity tristate is
+    generic(n: integer := 16);
+    port(
+        inData: in std_logic_vector(n-1 downto 0);
+        outData: out std_logic_vector(n-1 downto 0);
+        ctl: in std_logic
+    );
+end entity;
+
+architecture tristatearch of tristate is
+    begin
+        outData <= inData when ctl = '1'
+        else (others => 'Z');
+end architecture;
