7faebbcbb95a5723c4842370bb8677012c069113
ismailhelmy <38408394+ismailhelmy@users.noreply.github.com>
2019-04-26 13:43:26 +0200

    ALU is finished it outputs both flags and a 32 bit result.

--- ALU_EX.vhd
+++ ALU_EX.vhd
@@ -8,8 +8,8 @@ port(
     clock : in std_logic;
     reset : in std_logic;
     operation: in std_logic_vector(2 downto 0);
-    source : in std_logic_vector(15 downto 0);
-    destination : in std_logic_vector(15 downto 0);
+    src : in std_logic_vector(15 downto 0);
+    dest : in std_logic_vector(15 downto 0);
     shift_amount : in std_logic_vector(3 downto 0);
     output : out std_logic_vector(31 downto 0);
     flags : out std_logic_vector(2 downto 0)
@@ -20,75 +20,66 @@ end entity;
 Architecture behavioral of ArithmeticLogicUnit is
 
 
-signal result:std_logic_vector(31 downto 0);
+signal result, destination, source :std_logic_vector(31 downto 0);
 signal carry,zero,negative: std_logic;
 
 begin
     process(reset,clock)
         begin
+	
 	if (reset = '1') then
 		result <= "00000000000000000000000000000000";
 		carry<='0';
 		zero<='0';
 		negative<='0';
+		destination<="0000000000000000"&dest;
+		source <="0000000000000000"&src;
 	else 
 		if (alu_enable='1' and falling_edge(clock)) then
+				destination<="0000000000000000"&dest;
+				carry<='0';
+				zero<='0';
+				negative<='0';
+				source <="0000000000000000"&src;
 				if  (operation =   "001"  ) then
 					--ADDITION
 					result <=  std_logic_vector(to_signed(to_integer(unsigned(source)+unsigned(destination)),32));
 					carry <= result(16);
 				elsif operation =  "111" then
 					--SHR
-					--result <= "00000000000000000000000000000000";
 					result(15-to_integer(unsigned(shift_amount)) downto 0)<=destination(15 downto to_integer(unsigned(shift_amount)));
 					carry <= destination(to_integer(unsigned(shift_amount))-1);
 				elsif operation = "110" then
 					--SHL
-					--result <= "00000000000000000000000000000000";
 					result(15 downto  to_integer(unsigned(shift_amount)))<=destination(15 - to_integer(unsigned(shift_amount))downto 0);
-				    carry <= destination(16 - to_integer(unsigned(shift_amount)));
+				   	 carry <= destination(16 - to_integer(unsigned(shift_amount)));
 				elsif operation =   "101" then
 					--OR
 					result <= std_logic_vector(to_signed(to_integer(unsigned(source) or unsigned(destination)),32));
-					carry<='0';
 				elsif operation =   "100"  then
 					--AND
 					result <= std_logic_vector(to_signed(to_integer(unsigned(source) and unsigned(destination)),32));
-					carry<='0';
 				elsif operation =   "011" then
 					--MULTIPLICATION
 					result <= std_logic_vector(to_signed(to_integer(unsigned(source)*unsigned(destination)),32));
-					carry<='0';
 				elsif operation =   "010" then
 					--SUBTRACTION
 					result <= std_logic_vector(to_signed(to_integer(unsigned(destination)-unsigned(source)),32));
-					carry<=result(16);
 				elsif operation =   "111" then
-					result <= "0000000000000000"& not destination;
-					carry<='0';
+					result <= not destination;
 				else
 					result<="00000000000000000000000000000000";
-					carry<='0';
 				end if;
 		end if;
 	end if;
 end process;
 output<=result;
-flags(2)<= '1' when result = "00000000000000000000000000000000"
-	    else '0';
-flags(1)<= '1' when to_integer(unsigned(source))>to_integer(unsigned(destination))
-	    else '0';
-flags(0)<=carry;
+flags<= "101" when result(15 downto 0) = "0000000000000000" and result(16) = '1' else
+	"011" when to_integer(unsigned(source))>to_integer(unsigned(destination)) and result(16) = '1' else
+	"100" when result = "00000000000000000000000000000000" else
+	"010" when to_integer(unsigned(result(15 downto 0)))<0 else
+	"001" when (operation = "111" and  destination(to_integer(unsigned(shift_amount))-1) = '1') or 
+	(operation = "110" and destination(16 - to_integer(unsigned(shift_amount)))='1') or result(16) = '1';
 end Architecture;
-
-
--- opCode       operation     
---  000         addition
---  001         SHR        
---  010         SHL 
---  011         OR
---  100         AND 
---  101         MUL
---  110         SUB
---  111        
+     
 -- Z N C 
