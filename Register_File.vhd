953832f1ac06dfee3c13c9e7b27f5cf800237905
muhammad20 <muhammadahmad201196@hotmail.com>
2019-04-28 19:51:27 +0200

    ignore .wlf files

--- Register_File.vhd
+++ Register_File.vhd
@@ -10,18 +10,22 @@ Address1,Address2,Address3: in std_logic_vector( 2 downto 0);
 datain: in std_logic_vector(15 downto 0);
 dataout1,dataout2: out std_logic_vector(15 downto 0)
 );
-
-
 end entity;
 
 architecture arh2 of Register_File is  
 
-component mux8x1 is 
-generic( size: positive := 16);
-port(
-in0,in1,in2,in3, in4, in5, in6, in7 : in std_logic_vector(size-1 downto 0);
-s: in std_logic_vector(2 downto 0);
-c : out std_logic_vector(size-1 downto 0));
+component mux8x1 is 
+
+generic( size: positive := 16);
+
+port(
+
+in0,in1,in2,in3, in4, in5, in6, in7: in std_logic_vector(size-1 downto 0);
+
+s: in std_logic_vector(2 downto 0);
+
+c : out std_logic_vector(size-1 downto 0));
+
 end component;
 
 component decoder3x8 is 
@@ -63,7 +67,7 @@ t3: tristate generic map(16) port map (datain,outdata4,output3(3));
 t4: tristate generic map(16) port map (datain,outdata5,output3(4));
 t5: tristate generic map(16) port map (datain,outdata6,output3(5));
 t6: tristate generic map(16) port map (datain,outdata7,output3(6));
-t7: tristate generic map(16) port map (datain,outdata8,output4(7));
+t7: tristate generic map(16) port map (datain,outdata8,output3(7));
 r0: nbit_register generic map(16) port map (outdata1,clk,rst,output4(0),out1);
 r1: nbit_register generic map(16) port map (outdata2,clk,rst,output4(1),out2);
 r2: nbit_register generic map(16) port map (outdata3,clk,rst,output4(2),out3);
@@ -73,7 +77,7 @@ r5: nbit_register generic map(16) port map (outdata6,clk,rst,output4(5),out6);
 r6: nbit_register generic map(16) port map (outdata7,clk,rst,output4(6),out7);
 r7: nbit_register generic map(16) port map (outdata8,clk,rst,output4(7),out8);
 
-output4<= output1 or output2;
+output4 <= output1 or output2;
 
 d0: decoder3x8 port map(Address1,endec1,output1);
 d1: decoder3x8 port map(Address2,endec2,output2);
@@ -82,14 +86,22 @@ d2: decoder3x8 port map(Address3,endec3,output3);
 m0: mux8x1 generic map(16) port map(out1,out2,out3,out4,out5,out6,out7,out8,Address1,dataout1);
 m1: mux8x1 generic map(16) port map(out1,out2,out3,out4,out5,out6,out7,out8,Address2,dataout2);
 
-process(clk,endec1,endec2,endec3)
+process(clk, Write_Sig, Read_Sig)
 begin
-
-if(rising_edge(clk) and Write_Sig='1') 
-then endec3<='1';
-elsif (falling_edge(clk) and Read_Sig='1') 
-then endec2<='1';
-endec1<='1';
+if (rst = '1') then
+    dataout1 <= (others => '0');
+    dataout2 <= (others => '0');
+    else 
+    if(rising_edge(clk) and Write_Sig='1') 
+    then endec3<='1';
+        endec2 <='0';
+        endec1 <= '0';
+    elsif (falling_edge(clk) and Read_Sig='1') 
+        then endec2<='1';
+        endec1<='1';
+        endec3 <='0';
+    else 
+end if;
 end if;
 end process;
 
