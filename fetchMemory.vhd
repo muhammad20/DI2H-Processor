e2e426b47f8d97d078d7bb452d8dd9db6834855c
muhammad20 <muhammadahmad201196@hotmail.com>
2019-04-30 16:43:02 +0200

    integration

--- fetchMemory.vhd
+++ fetchMemory.vhd
@@ -8,64 +8,92 @@ port(	clock, reset, fetch_enable, mem_write, mem_read: in std_logic;
 	new_pc: in std_logic_vector(31 downto 0);
 	instruction_selectors: in std_logic_vector(1 downto 0);
 	memory1_location : in std_logic_vector(31 downto 0); -- instruction in memory[1] location
-	instruction: out std_logic_vector(31 downto 0);
 	program_counter: out std_logic_vector(31 downto 0)
 
 );
 end entity;
 
 Architecture fetching of FetchMemoryUnit is
---component memory_unit is
---    generic(n: integer := 32; m: integer := 20);
---    port(
---    data_bus: inout std_logic_vector(n-1 downto 0);
---    ram_addr: in std_logic_vector(m-1 downto 0);
---        clk, rst, mem_write, mem_read: in std_logic
---    );
---end component;
---component tristate is
---    generic(n: integer := 32);
---    port(
---        inData: in std_logic_vector(n-1 downto 0);
---        outData: out std_logic_vector(n-1 downto 0);
---        ctl: in std_logic
---    );
---end component;
---component mem_ram is
---
---    generic(n: integer; m: integer);
---
---    port(
---
---	address: in std_logic_vector(m-1 downto 0);
---
---	data_in: in std_logic_vector(n-1 downto 0);
---
---	clk, rst, wen: in std_logic;
---
---	data_out: out std_logic_vector(n-1 downto 0));
---
---end component;
---
---component FetchUnit is
---port( 	clock, fetch_enable, reset, pc_change_enable, int:in std_logic;
---	instruction_selectors : in std_logic_vector(1 downto 0);
---	memory1_location : in std_logic_vector(31 downto 0); -- instruction in memory[1] location
---	instruction_in : in std_logic_vector(31 downto 0);
---	new_pc : in std_logic_vector(31 downto 0);
---	program_counter: out std_logic_vector(31 downto 0);
---	instruction_out: out std_logic_vector(31 downto 0)
---);
---end component;
+
 signal dataBus : std_logic_vector(31 downto 0);
 signal fromMemory: std_logic_vector(31 downto 0);
 signal fromFetch: std_logic_vector(31 downto 0);
+signal fetch_dec_buffRst, fetch_dec_buffEn, dec_ex_buffRst, dec_ex_buffEn: std_logic;
+signal fetchedInstruction, bufferedInstruction: std_logic_vector(31 downto 0);
+signal regFileRead, regFileWrite: std_logic;
+signal readAddress1, readAddress2, writeAddress: std_logic_vector(2 downto 0);
+signal regInData: std_logic_vector(15 downto 0);
+signal opcode: std_logic_vector(4 downto 0);
+
+signal alu_src, wb_en, alu_en, jmp_en, memToReg, 
+memWrite, MemRead, push_sig, pop_sig, ret_rti: std_logic;
+
+signal jmp_type: std_logic_vector(1 downto 0);
+signal dec_ex_effective_address,
+	dec_ex_pc: std_logic_vector(19 downto 0);
+
+signal dec_ex_sp: std_logic_vector(31 downto 0);
+
+signal regFileOutData1, regFileOutData2: std_logic_vector(15 downto 0); 
+
+signal decExBuffDataIn, decExBuffDataOut: std_logic_vector(128 downto 0);
+
+signal dec_ex_src_addr, dec_ex_dst_addr: std_logic_vector(2 downto 0);
+signal dec_ex_src_val, dec_ex_dst_val: std_logic_vector(15 downto 0);
+signal sh_amount: std_logic_vector(3 downto 0);
+signal alu_result: std_logic_vector(31 downto 0);
+signal flags_result: std_logic_vector(2 downto 0); -- Z N C
 
 begin
+
+fetch_dec_buffRst <= reset;
+fetch_dec_buffEn <= '1';
+dec_ex_buffRst <= reset;
+dec_ex_buffEn <= '1';
+
+readAddress1 <= bufferedInstruction(10 downto 8);
+readAddress2 <= bufferedInstruction(7 downto 5);
+writeAddress <= bufferedInstruction(7 downto 5);
+
+decExBuffDataIn(128 downto 125) <= sh_amount;
+decExBuffDataIn(124 downto 120) <= opcode;
+decExBuffDataIn(119 downto 116) <= MemRead & MemWrite & memToReg & wb_en; 
+decExBuffDataIn(115 downto 114)  <= alu_en & alu_src;
+decExBuffDataIn(113 downto 110)  <= jmp_en & jmp_type & ret_rti;
+decExBuffDataIn(109 downto 90) <= dec_ex_effective_address;
+decExBuffDataIn(89 downto 70) <= dec_ex_pc;
+decExbuffDataIn(69 downto 38) <= dec_ex_sp;
+decExBuffDataIn(37 downto 19) <= dec_ex_src_addr & dec_ex_src_val;
+decExBuffDataIn(18 downto 0) <= dec_ex_dst_addr & dec_ex_dst_val;
+
+
 tristateBuffer: entity work.tristate port map(fromMemory,dataBus,fetch_enable);
-memory: entity work.memory_unit generic map(32,20) port map(fromMemory, fromFetch(19 downto 0),clock,reset,mem_write, mem_read);
-fetch: entity work.FetchUnit port map(clock, fetch_enable, reset,'1','0', instruction_selectors, memory1_location, dataBus, new_pc, fromFetch, instruction);
-fetchDecodeBuff: entity work.nbit_register generic map(32) port map();
+
+memory: entity work.memory_unit generic map(32,20) port map(fromMemory, 
+fromFetch(19 downto 0),clock,reset,mem_write, mem_read);
+
+fetch: entity work.FetchUnit port map(clock, fetch_enable, reset,'1','0', 
+instruction_selectors, memory1_location, dataBus, new_pc, 
+fromFetch, fetchedInstruction);
+
+fetchDecodeBuff: entity work.nbit_register generic map(32) port map(
+fetchedInstruction, 
+clock, fetch_dec_buffRst, fetch_dec_buffEn, bufferedInstruction);
+
+controlUnit: entity work.Control_Unit port map(bufferedInstruction(15 downto 11),
+opcode, alu_src, wb_en, alu_en, jmp_en, 
+memToReg, memWrite, MemRead, push_sig, pop_sig);
+
+registerFile: entity work.Register_File port map(regFileRead, regFileWrite, 
+clock, reset, readAddress1, readAddress2, writeAddress, 
+regInData, regFileOutData1, regFileOutData2);
+
+
+decodeExecuteBuff: entity work.nbit_register generic map(128) port map(decExBuffDataIn, clock, reset, decExBuffDataOut);
+ALU: entity work.ArithmeticLogicUnit port map(decExBuffDataOut(15), clock, reset,
+ decExBuffDataOut(121 downto 119), decExBuffDataOut(33 downto 18), decExBuffDataOut(15 downto 0),
+ decExBuffDataOut(127 downto 124), alu_result, flags_result);
+
 program_counter<=fromFetch;
 
 
